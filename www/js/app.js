231453d74b34f4aec279022467ce8e00477b10a3
gandhirajan <gandhirajan.n@gmail.com>
2016-06-23 15:51:23 +0530

    Cordova qr scanner sample app

---
+++ www/js/app.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	document.addEventListener("deviceready", onDeviceReady, false);
+});
+
+function onDeviceReady() {		
+     $('#scan').click( function() 
+		{
+		  alert('hi');
+		  cordova.plugins.barcodeScanner.scan(
+		  function (result) {
+			  alert("We got a barcode\n" +
+					"Result: " + result.text + "\n" +
+					"Format: " + result.format + "\n" +
+					"Cancelled: " + result.cancelled);			  
+		  }, 
+		  function (error) {
+			  alert("Scanning failed: " + error);
+		  });
+		}
+	 );
+}
